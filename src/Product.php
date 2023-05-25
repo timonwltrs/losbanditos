517fc02e91bb40ce099e68df50d2fe919e960b2a
timonwltrs <9012910@student.zadkine.nl>
2023-05-25 10:03:47 +0200

    Quantity button + Image URL
    Er staat naast de 'Add' button nu ook een quantity optie. Je kan nu ook bij het aanmaken van een product de URL bepalen als je op de foto klikt.

--- src/Product.php
+++ src/Product.php
@@ -10,20 +10,24 @@ class Product
     public string $description;
     public float $price;
     protected string $imageName;
+    protected string $prodUrl;
 
-    public function __construct(string $brand, string $description , float $price, string $imageName)
+    public function __construct(string $brand, string $description , float $price, string $imageName, string $prodUrl)
     {
         $this->brand = $brand;
         $this->description = $description;
         $this->price = $price;
         $this->imageName = $imageName;
-        //imageName = file name (for new product)
+        //imageName = file name (image (jpeg))
+        $this->prodUrl= $prodUrl;
+        //prodUrl = file name (php file)
+
     }
 
 
     public function addImage()
     {
-        return "<img src='../img/$this->imageName.jpeg' width=210 height=190>";
+        return "<a href='$this->prodUrl'><img src='../img/$this->imageName.jpeg' width=210 height=190></a>";
     }
 
     public function getBrand(): string
@@ -35,44 +39,35 @@ class Product
     {
         return
             "<div class='product'>".
-                $this->addImage() . "<br>" .
+             $this->addImage() . "<br>" .
                 "<ul>".
                     "<li>". $this->getBrand() ."</li>".
                     "<li>". $this->description."</li>".
                     "<li>". "$".$this->price ."</li>"
                 ."</ul>".
-            "</div>";
+                "<div class='btn'>".
+                "<button class='addcart-btn' type='submit'>Add</button>".
+                "<input class='quantity-btn' type='number' min='1' max='5'>".
+            "</div>"
+            ."</div>";
     }
 }
 
 //standard product display
-$product1 = new Product("Carhartt Wip", "Detroit Jacket 1983", "124.99", 'detroitjacket');
+$product1 = new Product("Carhartt Wip", "Detroit Jacket 1983", "124.99", 'detroitjacket','#');
 echo $product1->printProduct();
 
-$product2 = new Product("Carhartt Wip", "Detroit Jacket 1983", "84.99", 'detroitjacket');
+$product2 = new Product("Carhartt Wip", "Detroit Jacket 1983", "84.99", 'detroitjacket', '#');
 echo $product2->printProduct();
 
-$product3 = new Product("Carhartt Wip", "Detroit Jacket 1983", "69.99", 'detroitjacket');
+$product3 = new Product("Carhartt Wip", "Detroit Jacket 1983", "69.99", 'detroitjacket', '#');
 echo $product3->printProduct();
 
-$product4 = new Product("Carhartt Wip", "Detroit Jacket 1983", "179.99", 'detroitjacket');
+$product4 = new Product("Carhartt Wip", "Detroit Jacket 1983", "179.99", 'detroitjacket', '#');
 echo $product4->printProduct();
 
-$product5 = new Product("Carhartt Wip", "Detroit Jacket 1983", "124.99", 'detroitjacket');
+$product5 = new Product("Carhartt Wip", "Detroit Jacket 1983", "124.99", 'detroitjacket', '#');
 echo $product5->printProduct();
-
-$product6 = new Product("Carhartt Wip", "Detroit Jacket 1983", "74.99", 'detroitjacket');
-echo $product6->printProduct();
-
-$product7 = new Product("Carhartt Wip", "Detroit Jacket 1983", "119.49", 'detroitjacket');
-echo $product7->printProduct();
-
-$product8 = new Product("Carhartt Wip", "Detroit Jacket 1983", "134.99", 'detroitjacket');
-echo $product8->printProduct();
-
-$product9 = new Product("Carhartt Wip", "Detroit Jacket 1983", "24.99", 'detroitjacket');
-echo $product9->printProduct();
-
 
 ?>
 
@@ -89,5 +84,20 @@ echo $product9->printProduct();
     .product ul li{
         list-style-type: none;
         font-size: 18px;
+    }
+
+    .btn{
+        display: flex;
+    }
+
+    .addcart-btn{
+        color: black;
+        font-size: 15px;
+        background-color: gray;
+        width: 80%;
+    }
+
+    .quantity-btn{
+        background-color: #adadad;
     }
 </style>
