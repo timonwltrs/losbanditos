b38ed30798552718ec6e59b029a5cbbe20a92f02
timonwltrs <9012910@student.zadkine.nl>
2023-05-26 10:37:57 +0200

    First commit New-Start

--- index.php
+++ index.php
@@ -1,8 +1,39 @@
 <?php
 
-//composer
 require_once "vendor/autoload.php";
+require_once "include/smarty-4.3.0/libs/Smarty.class.php";
 
-//display navbar
-include 'includes/nav.html';
+use Losbanditos\User;
 
+$template = new Smarty();
+
+if(isset($_GET['action']))
+{
+    $action = $_GET['action'];
+}else{
+    $action = null;
+}
+
+
+switch($action){
+    case "registerform":
+        // formulier laten zien
+        $template->display('template/registratieform.tpl');
+        break;
+    case "register":
+        // $_POST['username'], $_POST['password1'], $_POST['password2']
+        if(!empty($_POST['username']) && !empty($_POST['password1']) && !empty($_POST['password2']))
+        {
+            $user = new User();
+            $user->register($_POST['username'], $_POST['password1'], $_POST['password2']);
+        }
+
+
+        $template->display('template/register.tpl');
+        break;
+    default:
+        $template->display('template/layout.tpl');
+
+}
+
+var_dump($user);
---
+++ template/registratieform.tpl
@@ -0,0 +1,21 @@
+{extends file="template/layout.tpl"}
+
+{block name="content"}
+<h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <form action="index.php?action=register" method="POST">
+        <div class="mb-3">
+            <label for="username" class="form-label">Email address</label>
+            <input type="text" name="username" class="form-control" id="username">
+        </div>
+        <div class="mb-3">
+            <label for="password1" class="form-label">Password</label>
+            <input type="password" name="password1" class="form-control" id="password1">
+        </div>
+        <div class="mb-3">
+            <label for="password2" class="form-label">Herhaal Password</label>
+            <input type="password" name="password2" class="form-control" id="password2">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Register</button>
+    </form>
+{/block}
