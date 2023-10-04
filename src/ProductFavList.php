8e75f325cf80667d5881ca2740b236623adb1107
timonwltrs <9012910@student.zadkine.nl>
2023-10-04 13:53:16 +0200

    favlist functie aangemaakt

--- src/ProductFavList.php
+++ src/ProductFavList.php
@@ -11,6 +11,25 @@ class ProductFavList
         return $this->favourites[] = $product;
     }
 
+    public static function getFavouriteList()
+    {
+        //wat selecteer ik
+        $columns = [
+            'products' => ['brand', 'description', 'price', 'imageName']
+        ];
+
+        //hier haal ik alles op uit de database
+        $products = Db::$db->select($columns);
+
+        Product::$products = [];
+
+        foreach ($products as $product)
+        {
+            $product = new Product($product['brand'],$product['description'],$product['price'],$product['imageName']);
+        }
+        return Product::$products;
+    }
+
     public function getFavourites(): array
     {
         return $this->favourites;
